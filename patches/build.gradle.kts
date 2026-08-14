63803a9f0c72794c524ca6594456b9d0c55ca820
Unknown King <callmeunik@gmail.com>
2026-08-14 15:22:50 +0530

    fix: Restore patches build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,6 +1,41 @@
-group = "com.callmeunik"   // apna naam
+group = "com.callmeunik"
 
-about {
-    name = "Unik Universal Patches"
-    description = "Universal patches for Morphe"
+patches {
+    about {
+        name = "Unik Universal Patches"
+        description = "Universal patches for Morphe"
+        source = "git@github.com:callmeunik/universal-patches.git"
+        author = "callmeunik"
+        contact = "na"
+        website = "na"
+        license = "GPLv3"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.add("-Xcontext-parameters")
+    }
+}
+
+val patchListGeneratorClasspath = configurations.create("patchListGeneratorClasspath")
+
+dependencies {
+    compileOnly(libs.gson)
+    patchListGeneratorClasspath(libs.gson)
+}
+
+tasks {
+    register<JavaExec>("generatePatchesList") {
+        description = "Build patch with patch list"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath + patchListGeneratorClasspath
+        mainClass.set("util.PatchListGeneratorKt")
+    }
+
+    publish {
+        dependsOn("generatePatchesList")
+    }
 }
