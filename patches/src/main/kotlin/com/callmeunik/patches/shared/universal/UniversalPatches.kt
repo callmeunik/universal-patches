44d1b6f86c81db813a3b888d04fa70a863027e34
Unknown King <callmeunik@gmail.com>
2026-08-14 14:52:05 +0530

    Update UniversalPatches.kt

--- patches/src/main/kotlin/com/callmeunik/patches/shared/universal/UniversalPatches.kt
+++ patches/src/main/kotlin/com/callmeunik/patches/shared/universal/UniversalPatches.kt
@@ -1,4 +1,4 @@
-package app.template.patches.shared.universal
+package com.callmeunik.patches.shared.universal
 
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
